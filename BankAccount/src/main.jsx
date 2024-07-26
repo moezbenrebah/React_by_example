e58e0c6990c6b1437a62f73f5aaa3faf23923076
moezbenrebah <benrebah.moez@gmail.com>
2024-07-27 00:05:44 +0100

    useReducer to implement a simple bank account logic

---
+++ BankAccount/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import BankAccount from './BankAccount'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BankAccount />
+  </React.StrictMode>,
+)
